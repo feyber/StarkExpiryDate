b2cc8d95b82a480087ae9c7a3662becc168a1314
0xquicksilver <rokhmadhidayad7@gmail.com>
2024-02-14 00:39:33 +0700

    add tailwindcss and update page

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,84 @@
-import Head from 'next/head';
-import FormDomain from '../components/FormDomain';
-import { useState } from 'react';
-import moment from 'moment';
+import Head from "next/head";
+import FormDomain from "../components/FormDomain";
+import { useState } from "react";
+import moment from "moment";
 
 const Home = () => {
-  const [domainData, setDomainData] = useState(null);
-  const [error, setError] = useState(null);
-  const [submittedDomain, setSubmittedDomain] = useState('');
+	const [domainData, setDomainData] = useState(null);
+	const [error, setError] = useState(null);
+	const [submittedDomain, setSubmittedDomain] = useState("");
 
-  const handleSubmit = async (domain) => {
-    try {
-      const response = await fetch(`https://api.starknet.id/domain_to_addr?domain=${domain}`);
-      if (!response.ok) {
-        throw new Error('TOLOL');
-      }
-      const jsonData = await response.json();
+	const handleSubmit = async (domain) => {
+		try {
+			const response = await fetch(
+				`https://api.starknet.id/domain_to_addr?domain=${domain}`
+			);
+			if (!response.ok) {
+				throw new Error("TOLOL");
+			}
+			const jsonData = await response.json();
 
-      // Convert Unix Epoch time to GMT time with moment.js
-      const gmtTime = moment.unix(jsonData.domain_expiry).utc().format('YYYY-MM-DD HH:mm:ss');
+			// Convert Unix Epoch time to GMT time with moment.js
+			const gmtTime = moment
+				.unix(jsonData.domain_expiry)
+				.utc()
+				.format("YYYY-MM-DD HH:mm:ss");
 
-      // Combines data with GMT time
-      const newData = { ...jsonData, gmt_time: gmtTime };
+			// Combines data with GMT time
+			const newData = { ...jsonData, gmt_time: gmtTime };
 
-      setDomainData(newData);
-      setSubmittedDomain(domain);
-      setError(null);
-      
-    } catch (error) {
-      setError(error.message);
-      setDomainData(null);
-      setSubmittedDomain('');
-    }
-  };
+			setDomainData(newData);
+			setSubmittedDomain(domain);
+			setError(null);
+		} catch (error) {
+			setError(error.message);
+			setDomainData(null);
+			setSubmittedDomain("");
+		}
+	};
 
-  return (
-    <div>
-      <Head>
-        <title>Domain Stark Expiry Date</title>
-        <meta name="description" content="Checker Domain Stark Expiry Date" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const sliceAddress = (address, length) => {
+		const end = address.substring(address.length - length)
+		return address.substring(0, length) + '...' + end;
+	}
 
-      <main>
-        <h1>Form Domain Stark</h1>
-        <FormDomain onSubmit={handleSubmit} />
-        {/* Show error messages if they occur */}
-        {error && <p>{error}</p>}
-        {/* Show the resulting data if any */}
-        {domainData && (
-          <div>
-            <h2>Domain Data:</h2>
-            <p>Domain: {submittedDomain}</p> 
-            <p>Address: {domainData.addr}</p>
-            <p>GMT Time: {domainData.gmt_time}</p>
-          </div>
-        )}
-      </main>
+	return (
+		<>
+			<Head>
+				<title>Domain Stark Expiry Date</title>
+				<meta
+					name="description"
+					content="Checker Domain Stark Expiry Date"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<section className="bg-white">
+					<div className="grid max-w-full py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+						<div className="mx-auto lg:col-span-12 pl-8 flex flex-col">
+							<h1 className="max-w-full mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl">
+								Domain <span className="underline font-serif text-red-500">.Stark</span> Checker
+							</h1>
+							<FormDomain onSubmit={handleSubmit} />
+							{/* Show error messages if they occur */}
+							{error && <span className="text-red-500">{error}</span>}
+							{/* Show the resulting data if any */}
+							{domainData && (
+								<div className="w-full xs:max-w-md">
+									<h2>Domain Data:</h2>
+									<p>Domain: {submittedDomain}</p>
+									<p>Address: {sliceAddress(domainData.addr, 10)}</p>
+									<p>GMT Time: {domainData.gmt_time}</p>
+								</div>
+							)}
+						</div>
+					</div>
+				</section>
+			</main>
 
-      <footer>
-        {/* Footer content */}
-      </footer>
-    </div>
-  );
-}
+			<footer>{/* Footer content */}</footer>
+		</>
+	);
+};
 
 export default Home;
